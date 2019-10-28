a0aff3109b797ebe26f8df83dfb60608e4654927
FRANCISCO\Francisco <fran.sempe@gmail.com>
2019-10-28 11:37:25 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from './services/photos.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-material7';
+export class AppComponent implements OnInit {
+  constructor(public photosService: PhotosService){
+
+  }
+  ngOnInit(){
+    this.photosService.getPhotos()
+      .subscribe(
+        photos => console.log(photos),
+        err => console.log(err)
+      )
+  }
 }
